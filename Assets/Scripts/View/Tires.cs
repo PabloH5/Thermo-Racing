f87c9b5732650083dd294b74073a3b6b17aa8887
Stevenjr2810 <gerson_steven.lopez@uao.edu.co>
2024-03-13 15:13:15 -0500

    feat: complete implementation HU1.1.4 of the following functionalities
    Choose tire type, the name changes according to the chosen tire, the specific heat (Cp) changes according to the tire type

---
+++ Assets/Scripts/View/Tires.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tires 
+{
+   public string typetires;
+   public Sprite tireSprite;
+   public int caloresEspecifico;
+}
