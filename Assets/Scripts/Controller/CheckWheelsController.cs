c317625fe569e3f7402041173440fd430feaf024
Jainsenberg <110469451+PabloH5@users.noreply.github.com>
2024-03-26 10:05:46 -0500

    fix: Adjust parameters of change wheels sprites

--- Assets/Scripts/Controller/CheckWheelsController.cs
+++ Assets/Scripts/Controller/CheckWheelsController.cs
@@ -16,10 +16,6 @@ public class CheckWheelsController : MonoBehaviour
     private ThermometerWheelsView thInflate;
     [SerializeField]
     private WheelToInflate wheelInflate;
-    [SerializeField]
-    private List<float> listFramesThermometer = new List<float>();
-    [SerializeField]
-    private float tolerance;
 
     void Start()
     {
@@ -39,30 +35,30 @@ public class CheckWheelsController : MonoBehaviour
     public void SwitchSpriteTH(int touchCount)
     {
         thInflate.SwitchSprite(touchCount);
+        Debug.Log(touchCount);
     }
     public void SwitchSpriteWH(int touchCount)
     {
-        if (touchCount <= 10)
+        if (touchCount <= 5)
         {
             wheelInflate.SwitchSprite(0);
         }
-        else if (touchCount > 10 && touchCount < 15)
+        else if (touchCount > 5 && touchCount < 7)
         {
             wheelInflate.SwitchSprite(1);
         }
-        else if (touchCount > 15 && touchCount < 20)
+        else if (touchCount > 7 && touchCount < 12)
         {
             wheelInflate.SwitchSprite(2);
         }
-        else if (touchCount >= 21 && touchCount < 24)
+        else if (touchCount >= 12 && touchCount < 18)
         {
             wheelInflate.SwitchSprite(3);
         }
-        else if (touchCount > 24 && touchCount <= 30)
+        else if (touchCount > 18 && touchCount <= 40)
         {
             wheelInflate.SwitchSprite(4);
             Debug.Log("BUUUUUUUUUUUUUM");
         }
-
     }
 }
