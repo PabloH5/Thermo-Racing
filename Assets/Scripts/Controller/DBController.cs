ad5b1927c31c0cc1353a284b802055ddc916d077
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-04-05 18:25:14 -0500

    feat: create DB connection to Neon Server
    Now, we can to able to switch the connection between localhost/remote host in Neon Servers.

--- Assets/Scripts/Controller/DBController.cs
+++ Assets/Scripts/Controller/DBController.cs
@@ -2,15 +2,34 @@ using Npgsql;
 using UnityEngine;
 using System;
 
+enum DBHostEnum {
+    local,
+    remote,
+}
+
 public static class DBController
 {
+    [SerializeField] private static DBHostEnum _dbHost;
 
-    private static string _dbConnectionString = "Host=localhost;Username=postgres;Password=admin123;Database=thermo-racing;Port=5432";
+    private static string _dbConnectionString;
 
     public static NpgsqlConnection EstablishConnectionDB()
     {
         try
         {
+            //_dbHost = DBHostEnum.remote;
+
+            if( _dbHost == DBHostEnum.local)
+            {
+                _dbConnectionString = "Host=localhost;Username=postgres;Password=admin123;Database=thermo-racing;Port=5432";
+            }
+            else
+            {
+                _dbConnectionString = "Host=ep-mute-wave-a4a7obkm.us-east-1.aws.neon.tech;Username=thermo-racing_owner;Password=*****+;Database=thermo-racing;Port=5432";
+            }
+
+            //Debug.Log(_dbConnectionString);
+
             var con = new NpgsqlConnection(_dbConnectionString);
             return con;
         }
