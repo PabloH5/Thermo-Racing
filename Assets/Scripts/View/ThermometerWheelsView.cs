4f75c30d10b19edfee202d2a897658f95d6b1a78
Jainsenberg <110469451+PabloH5@users.noreply.github.com>
2024-04-05 17:31:19 -0500

    doc: Docummented and create prefab of Check Wheels elements

--- Assets/Scripts/View/ThermometerWheelsView.cs
+++ Assets/Scripts/View/ThermometerWheelsView.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class ThermometerWheelsView : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+
+    [Tooltip("List of sprites of different states of the thermometer.")]
     [SerializeField]
     private List<Sprite> spritesTH;
 
