71ea7672463fcc9524fe600e3946d0e21a529a6b
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-04-05 18:30:15 -0500

    fix: added ARLL Manager in ARLL scene

--- Assets/Scripts/Controller/DBController.cs
+++ Assets/Scripts/Controller/DBController.cs
@@ -19,16 +19,16 @@ public static class DBController
         {
             //_dbHost = DBHostEnum.remote;
 
-            if( _dbHost == DBHostEnum.local)
+            if ( _dbHost == DBHostEnum.local)
             {
                 _dbConnectionString = "Host=localhost;Username=postgres;Password=admin123;Database=thermo-racing;Port=5432";
             }
             else
             {
-                _dbConnectionString = "Host=ep-mute-wave-a4a7obkm.us-east-1.aws.neon.tech;Username=thermo-racing_owner;Password=*****+;Database=thermo-racing;Port=5432";
+                _dbConnectionString = "Host=ep-mute-wave-a4a7obkm.us-east-1.aws.neon.tech;Username=thermo-racing_owner;Password=****;Database=thermo-racing;Port=5432";
             }
 
-            //Debug.Log(_dbConnectionString);
+            Debug.Log(_dbConnectionString);
 
             var con = new NpgsqlConnection(_dbConnectionString);
             return con;
