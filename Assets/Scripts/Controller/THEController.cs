35820f4ae993497406d8e5a458ba9c69916d4e9f
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-05-22 23:25:07 -0500

    feat: update reward modal information with a new interface and upload new Ignacio images

--- Assets/Scripts/Controller/THEController.cs
+++ Assets/Scripts/Controller/THEController.cs
@@ -41,10 +41,6 @@ public class THEController : MonoBehaviour
     [SerializeField] private ParticleSystem explotionParticleSystem;
     private bool hasExploted = false;
 
-
-    
-
-
     private float timer = 15;
     private float changeInterval = 0.5f;
     private int lastSpriteIndex = -1;
@@ -58,8 +54,12 @@ public class THEController : MonoBehaviour
 
     [Space(10)]
     [Header("Rewards")]
-    [SerializeField] private ItemAwardSO itemAwards;
-    [SerializeField] private Image imageReward;
+    [SerializeField] private AwardsController awardController;
+
+    public void Awake()
+    {
+        backgroundVideo.Prepare();
+    }
 
     // private List<String> answerPrefab = new();
     public bool IsReady
@@ -74,9 +74,9 @@ public class THEController : MonoBehaviour
     }
     void Start()
     {
-        
-        GetRandomAward();
-
+        backgroundVideo.Play();
+        backgroundVideo.Pause();
+        backgroundVideo.frame = 0;
         // CurrentSentence = randomSentence.SentenceAmount;
         for (int i = 0; i < randomSentence.SentenceAmount; i++)
         {
@@ -106,14 +106,14 @@ public class THEController : MonoBehaviour
         ShowSentences(CurrentSentence, true);
         backgroundVideo.Play();
         startGameBt.SetActive(false);
-        
+
     }
     private void Temporizator()
     {
         //Debug.Log($"-------VIDEO TIME: {backgroundVideo.time}");
         if (timer <= 0)
         {
-            
+
             SendNegativeFdB();
 
         }
@@ -172,7 +172,7 @@ public class THEController : MonoBehaviour
             IsReady = false;
             backgroundVideo.Stop();
 
-            GetRandomAward();
+            awardController.GetRandomAward();
 
             #endregion
         }
@@ -216,64 +216,5 @@ public class THEController : MonoBehaviour
         }
 
         negativeFeedBack.SetActive(true);
-    }
-
-    private void GetRandomAward()
-    {
-        // TEMPORARY LOGGIN
-        // ------------------------------------------------
-        LoggedUser.LogInUser("2222222", "test");
-        // ------------------------------------------------
- 
-
-        // 1. Get the possible items that user could win.
-        List<WheelModel> possibleWheelsRewards = InventoryRawModel.GetPossibleWheelsRewards(LoggedUser.UserCode);
-        List<ChassisModel> possibleChassisRewards = InventoryRawModel.GetPossibleChassisRewards(LoggedUser.UserCode);
-
-
-        if(possibleChassisRewards.Count == 0 && possibleWheelsRewards.Count == 0) {
-            Debug.Log("NO PUEDE GANAR RECOMPENSAS");
-            return;
-        }
-
-        // 2. Create a list for save all the possible items that can be awards.
-        List<object> itemList = new List<object>();
-
-        
-        itemList.AddRange(possibleWheelsRewards.Cast<object>());
-        itemList.AddRange(possibleChassisRewards.Cast<object>());
-
-        // 3. Order the items in a random way.
-        System.Random random = new System.Random();
-        itemList = itemList.OrderBy(x => random.Next()).ToList();
-
-        // 4. Obtain the user award
-        var userAward = itemList.First();
-       
-        // 5. Verify if the award will be "CHASSIS" or "WHEEL"
-        if(userAward.GetType() ==typeof( ChassisModel))
-        {
-            // Casting the variable item
-            ChassisModel chassisAward = (ChassisModel)userAward;
-
-            // Search into Scriptable Object the chassis won.
-            AwardInfo awardInfo = itemAwards.awards.Find(item => item.awardType == ItemType.CHASSIS && item.awardId == chassisAward.chassis_id);
-
-            // Change the sprite for the chassis won.
-            imageReward.sprite = awardInfo.awardSprite;
-            // Insert the new item in user inventory into DB.
-            InventoryUser.InsertChassisAwardToInventory(LoggedUser.UserCode, awardInfo.awardId);
-        }
-        else
-        {
-            WheelModel wheelAward = (WheelModel)userAward;
-            // Search into Scriptable Object the chassis won.
-            AwardInfo awardInfo = itemAwards.awards.Find(item => item.awardType == ItemType.WHEEL && item.awardId == wheelAward.wheel_id);
-
-            // Change the sprite for the wheel won.
-            imageReward.sprite = awardInfo.awardSprite;
-            // Insert the new item in user inventory into DB.
-            InventoryUser.InsertWheelAwardToInventory(LoggedUser.UserCode,awardInfo.awardId);
-        }
     }
 }
